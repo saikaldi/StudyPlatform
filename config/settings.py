4a4bb6d6769891557ce7bf1939f1fc85239fef6e
Aktan <aktanarynov566@gmail.com>
2024-12-27 23:51:18 +0600

    Nginx/Docker Added

--- config/settings.py
+++ config/settings.py
@@ -111,12 +111,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/app/static/"
 STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/app/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

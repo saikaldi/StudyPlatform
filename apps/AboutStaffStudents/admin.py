e5c63a27f0e5227e2fed9b71627b15da6de7a501
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2025-01-24 17:58:22 +0100

    Removed slug

--- apps/AboutStaffStudents/admin.py
+++ apps/AboutStaffStudents/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
 from .models import Subject, Graduate, Teacher, Feedback
 
+
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'created_at')
-    search_fields = ['name']
+    list_display = ("name", "created_at")
+    search_fields = ["name"]
+
 
 @admin.register(Graduate)
 class GraduateAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'score', 'slug', 'created_at')
-    search_fields = ['first_name', 'last_name']
+    list_display = ("first_name", "last_name", "score", "created_at")
+    search_fields = ["first_name", "last_name"]
+
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'subject', 'slug', 'created_at')
-    search_fields = ['first_name', 'last_name']
+    list_display = ("first_name", "last_name", "subject", "created_at")
+    search_fields = ["first_name", "last_name"]
+
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'slug', 'created_at')
-    search_fields = ['first_name', 'last_name', 'email']
+    list_display = ("first_name", "last_name", "email", "phone_number", "created_at")
+    search_fields = ["first_name", "last_name", "email"]
